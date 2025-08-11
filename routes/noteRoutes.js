81b67bbf98a266b18499fa0367e235f0a2f478a6
Oumar <dialloalphaoumar.taod@gmail.com>
2025-08-11 12:32:31 +0000

    suppression du controle sur la route get

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const noteCtrl = require('../controllers/noteController');
 
 router.post('/', noteCtrl.creerNote);
-router.get('/', noteCtrl.getNotes);
+router.get('/', getNotes);
 router.get('/:id', noteCtrl.getNote);
 router.put('/:id', noteCtrl.updateNote);
 router.delete('/:id', noteCtrl.deleteNote);
